960fe9b3b0d9f1c2499c24b5d0385922a6ca526a
JaZahn <jcleveng@fas.harvard.edu>
2015-09-16 23:45:05 -0400

    reverting files to clean state

--- js/tests/PlaylistSpec.js
+++ js/tests/PlaylistSpec.js
@@ -25,38 +25,9 @@ define(['Playlist', 'Song'], function(Playlist, Song){
       playlist = new Playlist();
       expect(playlist.playlist.length).not.toBe(0);
     });
-    describe('addSong', function(){
-      it('should add a song to the playlist array', function(){
-        var title = "title of song";
-        playlist.addSong(title);
-        expect(playlist.playlist[0].title).toBe(title);
-      });
-      it('should call updatePlaylist', function(){
-        spyOn(playlist, "updatePlaylist");
-        playlist.addSong("something something something");
-        expect(playlist.updatePlaylist).toHaveBeenCalled();
-      });
-    });
-    describe('removeSong', function(){
-      it("should remove the song from the array", function(){
-        playlist.addSong("something something something");
-        expect(playlist.playlist.length).toBe(1);
-        playlist.removeSong(0);
-        expect(playlist.playlist.length).toBe(0);
-      });
-      it('should call updatePlaylist', function(){
-        spyOn(playlist, "updatePlaylist");
-        playlist.addSong("something something something");
-        playlist.removeSong(0);
-        expect(playlist.updatePlaylist).toHaveBeenCalled();
-      });
-    });
-    describe('updatePlaylist', function(){
-      it('should set the sessionStorage for the object', function(){
-        expect(sessionStorage.getItem('playlist')).toBe('[]');
-        playlist.playlist.push(new Song('something new'));
-        playlist.updatePlaylist();
-        expect(sessionStorage.getItem('playlist')).toBe(JSON.stringify(playlist.playlist));
+    xdescribe('addSong', function(){
+      xit('should add a song to the playlist', function(){
+
       });
     });
   });
